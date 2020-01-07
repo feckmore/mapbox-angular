61a502b059ebfe177d3624a42214eb26b1f8c5bb
David Schultz <dschultz@arthrex.com>
2020-01-07 14:17:10 -0600

    basic functional mapbox

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,23 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { NgxMapboxGLModule } from 'ngx-mapbox-gl';
+import { environment } from 'src/environments/environment';
+
+const mapboxAccessToken = environment.mapboxAccessToken;
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    NgxMapboxGLModule.withConfig({
+      accessToken: mapboxAccessToken, // Optionnal, can also be set per map (accessToken input of mgl-map)
+      geocoderAccessToken: mapboxAccessToken // Optionnal, specify if different from the map access token, can also be set per mgl-geocoder (accessToken input of mgl-geocoder)
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
